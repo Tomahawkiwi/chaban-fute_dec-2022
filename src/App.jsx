87b930c38f7c6ab6790e7d42476aa4002d1e61f7
Tomahawkiwi <tlegendarme@gmail.com>
2022-12-19 16:07:22 +0100

    Project set up done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="h-screen w-screen bg-slate-400"></div>;
+}
+
+export default App;
